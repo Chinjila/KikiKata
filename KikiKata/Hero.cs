06dddd9d20459ebf2065b7b8cae85b4cd60d681e
marse <m@marsei.net>
2024-08-15 21:05:05 -0400

    8/15 study

---
+++ KikiKata/Hero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KikiKata
+{
+    public class Hero
+    {
+        public string Name = "Hero";
+        public string Position = "00";
+        public float Health = 100;
+        public float Damage = 5;
+        public int Experience = 0;
+        public Hero(string _name)
+        {
+                this.Name = _name;
+        }
+        public Hero():this("Hero")
+        {
+                
+        }
+    }
+}
---
+++ KikiKataTest/HeroTest.cs
@@ -0,0 +1,29 @@
+﻿using KikiKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KikiKataTest
+{
+    [TestFixture]
+    public class Hero_Class
+    {
+        [Test, Description("Hero should create a Hero")]
+        public void InitTest()
+        {
+            Hero myHero = new Hero();
+            Assert.AreEqual("Hero", myHero.Name);
+        }
+
+#pragma warning disable CS0183 // Suppress specific compiler warning, asserting a strongly typed property is that type will throw a warning
+        [Test, Description("Hero should have appropriate types for its properties")]
+        public void TypeTest()
+        {
+            Hero myHero = new Hero();
+            Assert.That(myHero.Health is float);
+        }
+#pragma warning restore CS0183
+    }
+}
