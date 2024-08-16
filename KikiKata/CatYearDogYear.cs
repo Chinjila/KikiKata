48c31d1f21fb33cf1e9d8b333a54a057eb8931d9
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-15 17:43:34 -0700

    Add project files.

---
+++ KikiKata/CatYearDogYear.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KikiKata
+{
+    public class Dinglemouse
+    {
+        public static int[] humanYearsCatYearsDogYears(int humanYears)
+        {
+            // Your code here!
+
+            int catYear = 0;
+
+            catYear = humanYears switch
+            {
+                <= 0 => 0,
+                1 => 15,
+                2 => 24,
+                >= 3 => (humanYears - 2) * 4 + 24
+            };
+            int dogYear = 0;
+
+            dogYear = humanYears switch
+            {
+                <= 0 => 0,
+                1 => 15,
+                2 => 24,
+                >= 3 => (humanYears - 2) * 5 + 24
+            };
+
+
+            return new int[] { humanYears, catYear, dogYear };
+        }
+    }
+}
---
+++ KikiKataTest/CatYearDogYearTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KikiKata;
+namespace KikiKataTest
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void One()
+        {
+            Assert.AreEqual(new int[] { 1, 15, 15 }, Dinglemouse.humanYearsCatYearsDogYears(1));
+        }
+
+        [Test]
+        public void Two()
+        {
+            Assert.AreEqual(new int[] { 2, 24, 24 }, Dinglemouse.humanYearsCatYearsDogYears(2));
+        }
+
+        [Test]
+        public void Ten()
+        {
+            Assert.AreEqual(new int[] { 10, 56, 64 }, Dinglemouse.humanYearsCatYearsDogYears(10));
+        }
+
+    }
+}
