303e56796809220d98eb1706308e6b08ea27a23a
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-20 17:14:19 -0700

    add grade solution

---
+++ KikiKata/Grade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KikiKata
+{
+    public class Grade
+    {
+        public static char Grader(double score)
+        => score switch
+        {
+            (> 1.0) or (< 0.6) => 'F',
+            >= 0.9 => 'A',
+            >= 0.8 => 'B',
+            >= 0.7 => 'C',
+            >= 0.6 => 'D'
+        };
+    }
+}
---
+++ KikiKataTest/GradeTest.cs
@@ -0,0 +1,22 @@
+﻿using KikiKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KikiKataTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase(0.7, ExpectedResult = 'C')]
+        [TestCase(0.9, ExpectedResult = 'A')]
+        [TestCase(0.6, ExpectedResult = 'D')]
+        public static char FixedTest(double score)
+        {
+            return Grade.Grader(score);
+        }
+    }
+}
